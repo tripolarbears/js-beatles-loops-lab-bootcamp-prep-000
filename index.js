ed2e5754ff4995ca2adf142cc39240595a38a7f1
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-25 22:46:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+  array.push(musicians[i] +" plays " + instruments[i])
+
+}
+  return array
+}
+
+function johnLennonFacts(array) {
+  var i=0
+  while (i<array.length) {
+    array[i] += '!!!';
+    i++;
+  }
+return array
+}
+
+function iLoveTheBeatles(num) {
+  var array = []
+  var i = 0;
+ 
+  do { array.push("I love the Beatles!"); i++;
+}
+while (num < 15);
+return array
+}
+
